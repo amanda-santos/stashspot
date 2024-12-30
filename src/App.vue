87ec82ce24d1ec6cc313ace5a8fb2e4fa392427f
Amanda Santos <amandasf0200@gmail.com>
2024-12-30 19:35:52 -0300

    feat: 🎉 setup project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import ToggleThemeButton from '@/components/ToggleThemeButton.vue'
+</script>
+
+<template>
+  <div>
+    <ToggleThemeButton />
+    Hello
+    <Button>Click me</Button>
+  </div>
+</template>
